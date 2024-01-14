8a461806fc47a096a06a9c90961dab0585bebdcc
Student <student@it.uekat.local>
2024-01-14 16:26:47 +0100

    photo

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import cv2
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
-
+import shutil
+import requests
 
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
@@ -9,7 +10,8 @@ hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 app = Flask(__name__)
 api = Api(app)
 
-class PeopleCounter(Resource):
+class PeopleCounterStatic(Resource):
+
     def get(self):
 
         image = cv2.imread('koncerty.jpg')
@@ -18,6 +20,30 @@ class PeopleCounter(Resource):
         return {'peopleCount': len(rects)}
 
 
-api.add_resource(PeopleCounter, '/')
+class PeopleCounterDynamicUrl(Resource):
+
+    def get(self):
+        url = request.args.get('url')
+        print('url',url)
+
+        res = requests.get(url, stream = True)
+        file_name = 'download-images-python.jpg'
+        if res.status_code == 200:
+            with open(file_name,'wb') as f:
+                 shutil.copyfileobj(res.raw, f)
+            print('Image sucessfully Downloaded: ',file_name)
+        else:
+            print('Image Couldn\'t be retrieved')
+
+        image = cv2.imread('koncerty.jpg')
+        image = cv2.resize(image, (700, 400))
+        (rects, weights) = hog.detectMultiScale(image, winStride=(4, 4), padding=(8, 8), scale=1.05)
+
+
+        return {'peopleCount': len(rects)}
+
+
+api.add_resource(PeopleCounterStatic, '/')
+api.add_resource(PeopleCounterDynamicUrl, '/dynamic')
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
